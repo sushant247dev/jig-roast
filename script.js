1dcf03a1b615bc7f8371d1bc2401b439d8b358a0
sushant247dev <sushant2010suzi@gmail.com>
2025-07-30 00:08:44 +0530

    Add files via upload
    updated code

--- script.js
+++ script.js
@@ -3,15 +3,13 @@ document.addEventListener("DOMContentLoaded", function () {
   const roastText = document.getElementById("roastText");
   const bgMusic = document.getElementById("bgMusic");
 
-  const hindiRoasts = [
-    "Tere jaise toh school ke toilet mein bhi nahi milte.",
-    "Tere IQ se zyada toh mere jute ke size hain.",
-    "Tujhe dekhke toh WiFi bhi signal chhod deta hai.",
-    "Tere jokes se zyada boring toh maths ka lecture hai.",
-    "Teri DP dekh ke toh phone bhi hang ho gaya.",
-    "Tu itna funny hai, ki log tujhe dekh ke rona chhod dete hain.",
-    "Jigyanshu, tu roast nahi, tohast hai 🤡"
-  ];
+  
+    const roastLines = [
+  "muummy: ladka hua ya ladki doctor:Chakka hua hai🧬😵",
+  "Tu itna boring hai, Netflix bhi tujhe 'Skip Intro' bol deta 📺⏩",
+  "Tera sense of humor toh Windows 98 jaisa hai — outdated aur crash-prone 💾💥",
+  "Tu vo typo hai jo autocorrect bhi ignore karta hai 🤖❌",
+  "Jigyanshu, tu roast nahi, tohast hai 🤡"];
 
   roastButton.addEventListener("click", () => {
     // Play audio from 1:05
